3b3bf4193628b8367dbf5b0db17136805c263777
luapicella <apc.luigi@gmail.com>
2021-11-10 10:52:59 +0100

    fix bug

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 import os.path
 import sys
-
+import pandas as pd
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -17,29 +17,6 @@ def pil_loader(path):
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
-
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
-        self.images_dataset = []
-        self.labels = []
-        self.labels_indx = []
-        self.unique_labels = []
-
-        file_path = "Caltech101" + "/" + self.split + ".txt"
-        print (file_path)
-        with open(file_path, "r" ) as fp:
-            for line in fp:
-                row = line.strip("\n")
-                label_tmp = row.split("/")[0]
-                if (label_tmp != "BACKGROUND_Google"):
-                    img = pil_loader(root + "/" + row)
-                    self.images_dataset.append(img)
-                    self.labels.append(label_tmp)
-
-        self.unique_labels = list(dict.fromkeys(self.labels))
-        for lab in self.labels:
-            self.labels_indx.append(list(self.unique_labels).index(lab))
-
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -49,17 +26,36 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
+        self.split = split # This defines the split you are going to use
+                           # (split files are called 'train.txt' and 'test.txt')
+
+        images_paths = []
+        labels = []
+
+        self.categories = os.listdir(self.root)
+        self.categories.remove("BACKGROUND_Google")
+
+        for path in open(f"./Caltech101/{self.split}.txt"):
+          path = path.replace("\n", "")
+          category = path.split("/")[0]
+          if(category != "BACKGROUND_Google"):
+            image_path = self.root + "/" + path
+            images_paths.append(image_path)
+            labels.append(self.categories.index(category))
+
+        self.data = pd.DataFrame(zip(images_paths, labels), columns = ["image_path", "label"])
+        
+
     def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-
-        image, label =   self.images_dataset[index], self.labels_indx[index]
+        image_path = self.data["image_path"].iloc[index]
+        image, label = pil_loader(image_path), self.data["label"].iloc[index]
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -72,5 +68,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = len(self.images_dataset)
+        length = len(self.data) # Provide a way to get the length (number of elements) of the dataset
         return length
